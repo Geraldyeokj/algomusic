3fdaa2f2aa04beb6702c92c6e6e8c3fa1c63f3ef
Geraldyeokj <geraldyeokj@gmail.com>
2023-11-07 19:25:05 -0600

    progress 1

--- sketch.js
+++ sketch.js
@@ -9,10 +9,12 @@ let p1
 /** @type {Pendulum} */
 let p2;
 
+const scaleIndexToNote = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"];
+
 // Create a new synth
 const synthA = new Tone.Synth().toDestination();
 const synthB = new Tone.Synth().toDestination();
-
+const synthC = new Tone.Synth().toDestination();
 
 synthA.triggerAttack(0); // Start the sound
 synthB.triggerAttack(0); // Start the sound
@@ -32,8 +34,8 @@ function setup() {
   btn.position(cnv.pos.x + 10, cnv.pos.y + height - 10 - btn.elt.offsetHeight)
 
   p1 = new Pendulum({ x: width / 2, y: 0 })
-  p2 = new Pendulum(p1.bob, undefined, 40, true)
-  p3 = new Pendulum(p2.bob, undefined, 40, true)
+  p2 = new Pendulum(p1.bob, 170, 40, true)
+  p3 = new Pendulum(p2.bob, 170, 40, true)
   let mouse = Mouse.create(cnv.elt)
   mouse.pixelRatio = pixelDensity();
   let mi = MouseConstraint.create(engine, { mouse })
@@ -41,20 +43,41 @@ function setup() {
 }
 
 
+function getBobTone(bobNumber) {
+  return scaleIndexToNote[document.getElementById(`bob${bobNumber}`).value] + document.getElementById(`scale`).value//gets the oninput value
+}
+
 function draw() {
   background(51);
   fill(255);
   noStroke();
   text("Use the mouse to move the bobs!", 10, 20)
-  text(`Pendulum 1 Speed: ${p1.bob.speed.toFixed(2)}}`, 10, 45)
+  text(`Pendulum 1\nSpeed: ${p1.bob.speed.toFixed(2)}, Tone: ${getBobTone(1)}`, 10, 45)
   //synthA.triggerAttack(p1.bob.speed.toFixed(2) * 400, "8n", Date.now());
-  text(`Pendulum 2 Speed: ${p2.bob.speed.toFixed(2)}}`, 10, 70)
+  text(`Pendulum 2\nSpeed: ${p2.bob.speed.toFixed(2)}, Tone: ${getBobTone(2)}`, 10, 95)
+  text(`Pendulum 3\nSpeed: ${p3.bob.speed.toFixed(2)}, Tone: ${getBobTone(3)}`, 10, 145)
   synthA.set({
-    frequency: p1.bob.speed.toFixed(2) * 200
+    volume: 0 - p1.bob.speed.toFixed(2) * 1
   });
   synthB.set({
-    frequency: p2.bob.speed.toFixed(2) * 200
+    volume: 0 - p2.bob.speed.toFixed(2) * 1
   });
+  synthC.set({
+    volume: 0 - p3.bob.speed.toFixed(2) * 1
+  });
+
+  p3.bob.frictionAir = 0.015 - 0.005 * document.getElementById(`bob3friction`).value
+
+
+  // synthA.set({
+  //   frequency: getBobTone(1)
+  // });
+  // synthB.set({
+  //   frequency: getBobTone(2)
+  // });
+  // synthC.set({
+  //   frequency: getBobTone(3)
+  // });
   
 
   p1.show();
@@ -64,7 +87,8 @@ function draw() {
   noFill();
   stroke(255)
   beginShape();
-  p2.history.forEach(pt => vertex(pt.x, pt.y))
+  //p2.history.forEach(pt => vertex(pt.x, pt.y))
+  p3.history.forEach(pt => vertex(pt.x, pt.y))
   endShape();
 
   Engine.update(engine)
@@ -73,6 +97,7 @@ function draw() {
 
 class Pendulum {
   constructor(posOrBody, len = random(70, 180), bobSize = 30, saveHistory = false) {
+    // len can be random:  len = random(70, 180)
     let x = posOrBody.x ?? posOrBody.position.x;
     let y = posOrBody.y ?? posOrBody.position.y;
     let key = "bodyA"
